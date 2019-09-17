9d35aa4bba3f8fa5b1aa3b7ca305dab03df87222
Wyxuch <stefankieubasa@gmail.com>
2019-09-17 16:10:13 +0200

    initial  commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './scss/App.scss';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div class='Content_box'>
+        <Header/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './scss/App.scss';
+
+function Nav() {
+    return (
+        <nav className='Navbar'>
+            <button>About me</button>
+            
+            <button>Contact</button>
+        </nav>
+    )
+}
+
+
+export default Nav;
