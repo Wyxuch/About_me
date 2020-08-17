956a2280a80f43f1286492a241056bf56678ea5a
Jarek <stefankieubasa@gmail.com>
2020-08-17 16:43:54 +0200

    Totally restarted project initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import './scss/App.scss';
-import Header from './Header';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div class='Content_box'>
-        <Header/>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
+
+const clicker = {
+  time: 100,
+  targetX: 1,
+  targetY: 1,
+  work: true,
+  start: function(){
+    this.work = true
+    setInterval(()=>{
+
+      if(this.work){
+        for(let i = 0; i < 1999; i++){
+        document.elementFromPoint(this.targetX, this.targetY).click()
+      }
+      }else{ 
+        clearInterval(this.start)
+      }
+    
+    },this.time)
+  },
+  stop: function(){
+    this.work = false
+  }
+}
+document.getElementsByTagName('body')[0].onclick = function(e){
+  let x = e.pageX;
+  let y = e.pageY;
+  console.log(x, y)
+}
