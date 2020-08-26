5c3dc335393f43729f0f14a500135f1119d26195
wyxuch <stefankieubsasa@gmail.com>
2020-08-26 18:30:13 +0200

    added constant text

--- src/constats.js
+++ src/constats.js
@@ -1,5 +1,10 @@
 const constants = {
   hello: "Hello World!!!",
+  drag: "Drag me!",
+  greetings: "I'm Jarek, nice to see you!",
+  position: "Full Stack Web Developer",
+  about:
+    "Bacon ipsum dolor amet short ribs ham landjaeger pork prosciutto flank chicken ground round jowl chislic strip steak ham hock pastrami. Burgdoggen picanha bacon shank cow chislic prosciutto pastrami doner capicola salami t-bone pig venison tenderloin. Jowl meatball pork belly ribeye, turducken kevin chuck t-bone flank sausage drumstick rump ball tip tenderloin. Capicola sirloin bacon beef, brisket short loin short ribs buffalo salami strip steak shank doner venison kielbasa boudin. Biltong cupim kevin ribeye. Ham hock landjaeger filet mignon, kevin buffalo brisket salami meatball pork belly meatloaf turkey ham pig. Pancetta kielbasa biltong frankfurter, filet mignon ground round beef sausage venison tenderloin andouille cupim. Doner porchetta meatloaf cow chislic sausage.",
 };
 
 export default constants;
