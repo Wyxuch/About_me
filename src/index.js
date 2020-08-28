30547dda64310093e4d083b503ad5ed1850b65cc
wyxuch <stefankieubsasa@gmail.com>
2020-08-28 20:52:02 +0200

    removed devtools

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from "./components/App";
 import rootReducer from "./reducers";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 render(
   <Provider store={store}>
